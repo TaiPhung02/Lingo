437f25028db4d65b5b0b4a03554d1d10b9df3802
TaiDev <2051063445@e.tlu.edu.vn>
2025-04-13 15:53:50 +0700

    feat: footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,9 +1,55 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/vn.svg"
+            alt="Vietnamese"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Vietnamese
+        </Button>
+
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/kr.svg"
+            alt="Korean"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Korean
+        </Button>
+
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/cn.svg"
+            alt="Chinese"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Chinese
+        </Button>
+
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/en.svg"
+            alt="English"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          English
+        </Button>
+      </div>
     </footer>
   );
 };
