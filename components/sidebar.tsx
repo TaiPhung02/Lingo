1579f69dd2ac95d364d02e3543e3450317a52ce3
TaiDev <2051063445@e.tlu.edu.vn>
2025-04-14 09:53:21 +0700

    feat: Main Layout

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { cn } from "@/lib/utils";
 
-const Sidebar = () => {
-  return <div className="bg-blue-500 h-full w-[256px] fixed">Sidebar</div>;
+type Props = {
+  className?: string;
+};
+
+export const Sidebar = ({ className }: Props) => {
+  return (
+    <div
+      className={cn(
+        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        className
+      )}>
+      Sidebar
+    </div>
+  );
 };
 
 export default Sidebar;
