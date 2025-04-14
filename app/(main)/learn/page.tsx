d74f1b954233f377ee5ec3d3a65abb8db0af9d9f
TaiDev <2051063445@e.tlu.edu.vn>
2025-04-14 14:19:58 +0700

    feat: Learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import React from "react";
+import Header from "./header";
+import { UserProgress } from "@/components/user-progress";
 
 const LearnPage = () => {
   return (
-    <div>LearnPage</div>
-  )
-}
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Korean", imageSrc: "/kr.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubsription={false}
+        />
+      </StickyWrapper>
 
-export default LearnPage
+      <FeedWrapper>
+        <Header title="Korean" />
+      </FeedWrapper>
+    </div>
+  );
+};
+
+export default LearnPage;
