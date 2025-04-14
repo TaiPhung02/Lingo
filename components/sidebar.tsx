1802ab0fa3fb1c7b42c5bc965e77cf946f504fe2
TaiDev <2051063445@e.tlu.edu.vn>
2025-04-14 10:26:50 +0700

    feat: Sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,10 @@
+import Link from "next/link";
+import Image from "next/image";
+
 import { cn } from "@/lib/utils";
+import { SidebarItem } from "./sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
@@ -8,10 +14,47 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
       )}>
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/logo.svg" height={40} width={40} alt="Logo" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo.
+          </h1>
+        </div>
+      </Link>
+
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+        <SidebarItem
+          label="Pronounce"
+          href="/pronounce"
+          iconSrc="/pronounce.svg"
+        />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/ranking.svg"
+        />
+        <SidebarItem label="Quests" href="/quests" iconSrc="/mission.svg" />
+        <SidebarItem label="Store" href="/store" iconSrc="/store.svg" />
+        <SidebarItem label="Profile" href="/profile" iconSrc="/profile.svg" />
+        <SidebarItem label="More" href="/more" iconSrc="/more.svg" />
+      </div>
+
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+
+        <ClerkLoaded>
+          <UserButton
+          // afterSignOutUrl="/"
+          />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
