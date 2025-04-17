65a9e38ac9077393f4baa05a73c7d6a706192b67
TaiDev <2051063445@e.tlu.edu.vn>
2025-04-17 17:14:53 +0700

    feat: Admin Lessons

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -11,6 +11,10 @@ import { UnitList } from "./unit/list";
 import { UnitCreate } from "./unit/create";
 import { UnitEdit } from "./unit/edit";
 
+import { LessonList } from "./lesson/list";
+import { LessonCreate } from "./lesson/create";
+import { LessonEdit } from "./lesson/edit";
+
 const dataProvider = simpleRestProvider("/api");
 
 const App = () => {
@@ -29,6 +33,14 @@ const App = () => {
         list={UnitList}
         create={UnitCreate}
         edit={UnitEdit}
+        recordRepresentation="title"
+      />
+
+      <Resource
+        name="lessons"
+        list={LessonList}
+        create={LessonCreate}
+        edit={LessonEdit}
         recordRepresentation="title"
       />
     </Admin>
