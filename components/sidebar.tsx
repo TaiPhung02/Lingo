9038af9863966d2147de9246be98ff371515e3f7
TaiDev <2051063445@e.tlu.edu.vn>
2025-04-21 13:28:52 +0700

    fix: change logo

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -16,8 +16,9 @@ export const Sidebar = ({ className }: Props) => {
       className={cn(
         "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
-      )}>
-      <Link href="/learn">
+      )}
+    >
+      <Link href="/">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
           <Image src="/logo.svg" height={40} width={40} alt="Logo" />
           <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
